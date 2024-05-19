bd32cdd0ca3beac232d98edd7fd72dd234d23641
DanubiaM <dubamacedoo@gmail.com>
2024-05-19 17:13:05 -0400

    create use case to show balance from account

---
+++ app/src/Application/Usecase/BalanceAccount.php
@@ -0,0 +1,16 @@
+<?php
+
+class BalanceAccount{
+
+    public function __construct()
+    {
+        //TODO: call repository
+    }
+
+
+    public function execute(string $idAccount){
+        // GET ACCOUNT BY REPOSITORY
+        // CALL FUNCTION TO SHOW BALANCEs
+        // SAVE IN DATABASE
+    }
+}
